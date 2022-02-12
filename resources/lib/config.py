87bcb1357af50a29b806a525f626f4a0ae80fd40
Matthias Reichl <hias@horus.com>
2022-02-12 19:03:23 +0100

    don't use HOME or XDG_ environment variables
    Kodi messes with the HOME environment variable in
    xbmc/platform/posix/filesystem/SMBFile.cpp, temporarily changing
    it to the .kodi folder:
    https://github.com/xbmc/xbmc/commit/2b1402b876dd8d5f377cbcac6a293ed04863e246
    
       setenv("HOME", CSpecialProtocol::TranslatePath("special://home").c_str(), 1);
    
    This causes LE settings to fail miserably if it starts at the wrong
    moment as it can't find the .cache and .config folders. eg:
    
    ERROR <general>: SETTINGS: system.set_hostname # FileNotFoundError(2, 'No such file or directory')
    ...
    Exception value : [Errno 2] No such file or directory: '/storage/.kodi/.cache/hostname'
    
    Likewise XDG_ environment variables might get changed by desktop or
    session managers.
    
    oe.py and defaults.py already allow setting the .cache and .config locations
    via the CONFIG_CACHE and USER_CONFIG environment variables so use the same
    mechanism in config.py as well.
    
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- resources/lib/config.py
+++ resources/lib/config.py
@@ -6,14 +6,11 @@ import os_tools
 
 OS_RELEASE = os_tools.read_shell_settings('/etc/os-release')
 
-HOME = os.environ.get('HOME', '/storage')
-XDG_CACHE_HOME = os.environ.get('XDG_CACHE_HOME', os.path.join(HOME, '.cache'))
-XDG_CONFIG_HOME = os.environ.get(
-    'XDG_CONFIG_HOME', os.path.join(HOME, '.config'))
-XDG_RUNTIME_DIR = os.environ.get('XDG_RUNTIME_DIR' '/run')
+CONFIG_CACHE = os.environ.get('CONFIG_CACHE', '/storage/.cache')
+USER_CONFIG = os.environ.get('USER_CONFIG', '/storage/.config')
 
-HOSTNAME = os.path.join(XDG_CACHE_HOME, 'hostname')
-HOSTS_CONF = os.path.join(XDG_CONFIG_HOME, 'hosts.conf')
+HOSTNAME = os.path.join(CONFIG_CACHE, 'hostname')
+HOSTS_CONF = os.path.join(USER_CONFIG, 'hosts.conf')
 
-REGDOMAIN_CONF = os.path.join(XDG_CACHE_HOME, 'regdomain.conf')
+REGDOMAIN_CONF = os.path.join(CONFIG_CACHE, 'regdomain.conf')
 SETREGDOMAIN = '/usr/lib/iw/setregdomain'
